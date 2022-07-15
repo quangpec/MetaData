39c5dd1b6acad5aea16cd95d4d0dbac3fc146dc0
quanglnFX13841 <quanglnq0305@gmail.com>
2022-07-15 23:36:57 +0700

    first commit

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => {
-    app.listen(process.env.PORT || 3000);
+    app.listen(process.env.port || 3000);
   })
   .catch(err => {
     const error = new Error(err);
